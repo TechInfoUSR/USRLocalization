c0c455fa5c5a168b94f80958591e3c5847b6df94
Hanumanth <hanumanthpujar72@gmail.com>
2024-07-01 18:11:50 +0530

    USRcommitno10

--- src/main/java/NormalFlowForEmployee/TestClass.java
+++ src/main/java/NormalFlowForEmployee/TestClass.java
@@ -17,50 +17,118 @@ public class TestClass {
 	        WebDriver driver = new ChromeDriver();
 
 	        // Navigate to the webpage with the dropdown
-	        driver.get("https://greythrpms.co/solutions/login?invalid=1");
+	        driver.get("https://test.okrstars.com/solutions/login?invalid=1");
 	        Thread.sleep(2000);
 	        driver.manage().window().maximize();
-	        driver.findElement(By.xpath("//*[@id=\"hello\"]/div[2]/input")).sendKeys("manjunath@gmail.com");
+	        driver.findElement(By.xpath("//*[@id=\"hello\"]/div[2]/input")).sendKeys("ranganath@okrstars.com");
 	        Thread.sleep(2000);
 	        driver.findElement(By.xpath("//*[@id=\"hello\"]/div[3]/input")).sendKeys("talent");
 	        Thread.sleep(2000);
 	        driver.findElement(By.xpath("//*[@id=\"hello\"]/div[4]/button")).click();
 	        Thread.sleep(2000);
-	        driver.findElement( By.xpath("//a[@class='dropdown-toggle']")).click();
-	        Thread.sleep(1000);
-            driver.findElement(By.xpath("//a[contains(text(),'Testing 1706 Normalization ')]")).click();
-            Thread.sleep(5000);
-//            driver.findElement(By.xpath("(//span[@class=\"ml-4\"])[1]")).click();
+//	        driver.findElement( By.xpath("//a[@class='dropdown-toggle']")).click();
+//	        Thread.sleep(1000);
+//          driver.findElement(By.xpath("//a[contains(text(),'Testing 1706 Normalization ')]")).click();
+//          Thread.sleep(1000);
+//          driver.findElement(By.xpath("(//span[@class=\"ml-4\"])[1]")).click();
             
             
-//            WebElement listViewCard = driver.findElement(By.cssSelector(".list-view-card"));
+            
+            
+            WebElement element = driver.findElement(By.xpath("(//div[@class=\"card list-view-card\"])[1]"));
+            // Use JavaScriptExecutor to click on the element
+            JavascriptExecutor executor = (JavascriptExecutor) driver;
+            executor.executeScript("arguments[0].click();", element);
+            Thread.sleep(1000);
+            
+            
+            
+            
+            
+//            WebElement rangeInput = driver.findElement(By.cssSelector("input[type='range']")); 
+//            int progress = 50; // Example: setting progress to 50%
 //
-//            // Perform actions on the element
-//            listViewCard.click();
-//            Thread.sleep(2000);
-            WebElement listViewCard = driver.findElement(By.xpath("//*[contains(@class, 'list-view-card')]"));
+//            // Cast WebDriver to JavascriptExecutor
+//            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+//
+//            // Update the value of the range input and trigger the change event
+//            String script = "arguments[0].value = arguments[1];" +
+//                            "arguments[0].dispatchEvent(new Event('change'));";
+//            jsExecutor.executeScript(script, rangeInput, progress);
+//
+//            // Optionally, update the progress text (if needed)
+//            WebElement progressText = driver.findElement(By.cssSelector(".risk"));
+//            jsExecutor.executeScript("arguments[0].innerText = arguments[1];", progressText, String.format("%d%% Complete", progress));
+//
+//            // Close the browser (optional)
+            // driver.quit();
+            
+            
+            
+            updateProgressBar(driver, "(//input[@type=\"range\"])[1]", 50);
+            Thread.sleep(1000);
+            // Update progress for the second progress bar
+            updateProgressBar(driver, "(//input[@type=\"range\"])[2]", 75);
 
-            // Perform actions on the element
-            listViewCard.click();
-            
-            
-	        // Locate the dropdown element
-//	        WebElement dropdownContainer = driver.findElement(By.cssSelector(".dropdown-container"));
-//
-//	        // Use JavaScript to get the computed style of the ::after pseudo-element
-//	        JavascriptExecutor js = (JavascriptExecutor) driver;
-//	        String pseudoElementContent = (String) js.executeScript(
-//	            "return window.getComputedStyle(arguments[0], '::after').getPropertyValue('content');",
-//	            dropdownContainer
-//	        );
-//
-//	        // Print the content of the pseudo-element
-//	        System.out.println("Pseudo-element content: " + pseudoElementContent);
-//
-//	        // Interact with the dropdown (e.g., clicking to open it)
-//	        dropdownContainer.click();
-
-	        // Close the browser
+            Thread.sleep(1000);
+            // Perform actions after the progress bar is complete
+            System.out.println("Progress bar is complete!");
 	        driver.quit();
+	    }    
+	    
+	    
+	    
+	    private static void updateProgressBar(WebDriver driver, String cssSelector, int progress) {
+	    	
+	    	  WebElement rangeInput = driver.findElement(By.xpath(cssSelector)); 
+//            int progress = 50; // Example: setting progress to 50%
+
+            // Cast WebDriver to JavascriptExecutor
+            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+
+            // Update the value of the range input and trigger the change event
+            String script = "arguments[0].value = arguments[1];" +
+                            "arguments[0].dispatchEvent(new Event('change'));";
+            jsExecutor.executeScript(script, rangeInput, progress);
+
+            // Optionally, update the progress text (if needed)
+            WebElement progressText = driver.findElement(By.cssSelector(".risk"));
+            jsExecutor.executeScript("arguments[0].innerText = arguments[1];", progressText, String.format("%d%% Complete", progress));
+
+            // Close the browser (optional)
+            // driver.quit();
+	    
 	    }
+	    
+	    
+	    
+	    
+	    
+	    
+            // Method to update progress bar based on CSS selector and progress value
+//            private static void updateProgressBar(WebDriver driver, String cssSelector, int progress) {
+//                // Locate the range input element
+//                WebElement rangeInput = driver.findElement(By.xpath(cssSelector));
+//
+//                // Cast WebDriver to JavascriptExecutor
+//                JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+//
+//                // Update the value of the range input and trigger the change event
+//                String script = "arguments[0].value = arguments[1];" +
+//                                "arguments[0].dispatchEvent(new Event('change'));";
+//                jsExecutor.executeScript(script, rangeInput, progress);
+//
+//                // Optionally, update the progress text (if needed)
+//                WebElement progressText = rangeInput.findElement(By.xpath("./following-sibling::div[@class='risk']"));
+//                jsExecutor.executeScript("arguments[0].innerText = arguments[1];", progressText, String.format("%d%% Complete", progress));
+//            }
+//            
+            
+            
+            
+            
+
+     
+
+	 
 	}
--- src/main/java/NormalFlowForEmployee/emp_assesment_Submission.java
+++ src/main/java/NormalFlowForEmployee/emp_assesment_Submission.java
@@ -3,12 +3,16 @@ package NormalFlowForEmployee;
 import java.util.Properties;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class emp_assesment_Submission {
 	  private WebDriver driver;
       private Properties prop;
-    
+     
     	private By dropdownToggle = By.xpath("//a[@class='dropdown-toggle']");
     	  private By goalCycleLink(String cycleName) {
               return By.xpath("//a[contains(text(),'" + cycleName + "')]");
@@ -27,8 +31,86 @@ public class emp_assesment_Submission {
               driver.findElement(By.xpath("//span[@class=\"ml-4\"]")).click();
               Thread.sleep(2000);
               
+              
+              
+              
+          //  FOR Clicking on Drop down  
+              WebElement element = driver.findElement(By.xpath("(//div[@class=\"card list-view-card\"])[1]")); 
+              // Use JavaScriptExecutor to click on the element
+              JavascriptExecutor executor = (JavascriptExecutor) driver;
+              executor.executeScript("arguments[0].click();", element);
+              Thread.sleep(3000);
+            
+              
+              
+          //  FOR Drgging the progress bar 
+//           WebElement rangeInput = driver.findElement(By.cssSelector("input[type='range']")); 
+//           int progress = 70; // Example: setting progress to 50%
+//
+//           // Cast WebDriver to JavascriptExecutor
+//           JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+//
+//           // Update the value of the range input and trigger the change event
+//           String script = "arguments[0].value = arguments[1];" +
+//                           "arguments[0].dispatchEvent(new Event('change'));";
+//           jsExecutor.executeScript(script, rangeInput, progress);
+//
+//           // Optionally, update the progress text (if needed)
+//           WebElement progressText = driver.findElement(By.cssSelector(".risk"));
+//           jsExecutor.executeScript("arguments[0].innerText = arguments[1];", progressText, String.format("%d%% Complete", progress));
+//           Thread.sleep(3000);
+           
+           updateProgressBar(driver, "(//input[@type=\"range\"])[1]", 50);
+           Thread.sleep(1000);
+           updateProgressBar(driver, "(//input[@type=\"range\"])[2]", 75);
+           Thread.sleep(2000); 
+           updateProgressBar(driver, "(//input[@type=\"range\"])[3]", 25);
+           Thread.sleep(2000); 
+           updateProgressBar(driver, "(//input[@type=\"range\"])[4]", 15);
+           Thread.sleep(2000); 
+           
+           
+//           Increase the Number in 2nd String and Last String for Every Time while calling the Method
+           EmployeeComment(driver,"(//img[@class=\"dropdown-toggle\"])[5]","Employee comment", "(//a[@class=\"view-link mt-h cs-modal-btn ng-binding ng-scope\"]) [1]");
+           Thread.sleep(2000);
+           
+           EmployeeComment(driver,"(//img[@class=\"dropdown-toggle\"])[6]","Employee comment", "(//a[@class=\"view-link mt-h cs-modal-btn ng-binding ng-scope\"]) [2]");
+           Thread.sleep(2000);
+           
+           
+           driver.findElement(By.xpath("//a[@ng-click=\"submitSelfPerformance(1)\"]")).click();
+           Thread.sleep(2000); 
+//           driver.findElement(By.xpath("//button[contains(text(),'OK')]")).click();
+           Thread.sleep(2000); 
+           
           }
-    	     
+    	  private static void updateProgressBar(WebDriver driver, String cssSelector, int progress) {  
+	    	  WebElement rangeInput = driver.findElement(By.xpath(cssSelector)); 
+            // Cast WebDriver to JavascriptExecutor
+            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+            // Update the value of the range input and trigger the change event
+            String script = "arguments[0].value = arguments[1];" +
+                            "arguments[0].dispatchEvent(new Event('change'));";
+            jsExecutor.executeScript(script, rangeInput, progress);
+            // Optionally, update the progress text (if needed)
+            WebElement progressText = driver.findElement(By.cssSelector(".risk"));
+            jsExecutor.executeScript("arguments[0].innerText = arguments[1];", progressText, String.format("%d%% Complete", progress));
+
+
+	    }
+    	  private static void EmployeeComment(WebDriver driver, String cssSelector, String Empcmt, String ReviewBTN) throws InterruptedException { 
+    		  driver.findElement(By.xpath(cssSelector)).click();
+    		  Thread.sleep(2000);
+    		  driver.findElement(By.xpath(ReviewBTN)).click();
+    		  Thread.sleep(2000);
+    		  String goal_comments1 = "goal_comments1";    	
+    		  driver.findElement(By.id(goal_comments1)).sendKeys(Empcmt);
+    		  Thread.sleep(2000);
+    		  driver.findElement(By.xpath("(//button[contains(text(),'Submit')])[3]")).click();
+    		  Thread.sleep(5000);
+    		  driver.findElement(By.xpath("(//button[contains(text(),'Close')])[1]")).click();
+    		  Thread.sleep(2000);
+    	  }
     }
 
 
