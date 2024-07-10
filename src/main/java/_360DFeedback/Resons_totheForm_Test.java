cb165b03a4e464a16d7b52ef6d39d62363e2d64f
Hanumanth <hanumanthpujar72@gmail.com>
2024-07-10 17:57:24 +0530

    USRcommitno12

--- src/main/java/_360DFeedback/Resons_totheForm_Test.java
+++ src/main/java/_360DFeedback/Resons_totheForm_Test.java
@@ -3,8 +3,6 @@ package _360DFeedback;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
@@ -14,37 +12,34 @@ import ConfigReder.ConfigpropReader;
 import Factory.DriverFactory;
 
 public class Resons_totheForm_Test {
-	  DriverFactory df;
-	    ConfigpropReader cp;
-	    Properties prop; 
-	    WebDriver driver;
-	    Respons_to_the_Form Respons_to_the_Form;
+
+		    DriverFactory df;
+		    ConfigpropReader cp;
+		    Properties prop; 
+		    WebDriver driver;
+		    Respons_to_the_Form Respons_to_the_Form;
+		    
+		    @BeforeTest
+		    void setUp() throws IOException, InterruptedException 
+		    {
+		        cp = new ConfigpropReader();
+		        prop = cp.initLangProp("normal");
+		        df = new DriverFactory();
+		        driver = df.initDriver("chrome", prop);
+		       
+		        Respons_to_the_Form = new Respons_to_the_Form(driver, prop);
 	    
-	    @BeforeTest
-	    void setUp() throws IOException, InterruptedException 
-	    {
-	        cp = new ConfigpropReader();
-	        prop = cp.initLangProp("normal");
-	        df = new DriverFactory();
-	        driver = df.initDriver("chrome", prop);
-	       
-	        Respons_to_the_Form = new Respons_to_the_Form(driver, prop);
-        
-	    
-	    }
+		    }
 
-	    @Test
-	    void send360form() throws InterruptedException, IOException {
-	    	Respons_to_the_Form.login(("Empusername"),("Empuserpass"));
-	    	
+		    @Test
+		    void send360form() throws InterruptedException, IOException {
+		    	Respons_to_the_Form.login(("Empusername"),("Empuserpass"));
+		    	
+		    }
 
-	
-	    }
+		    @AfterClass
+		    void teardown() {
+			driver.quit();
+		    }
 
-
-	    @AfterClass
-	    void teardown() {
-		driver.quit();
-	    }
-
-}
+	}
--- src/main/java/_360DFeedback/Respons_to_the_Form.java
+++ src/main/java/_360DFeedback/Respons_to_the_Form.java
@@ -1,9 +1,8 @@
 package _360DFeedback;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Properties;
-
+import java.util.List;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -11,118 +10,110 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class Respons_to_the_Form {
-	 private WebDriver driver;
-     private Properties prop;
-     
-     public Respons_to_the_Form(WebDriver driver, Properties prop) 
-     {
-         this.driver = driver;
-         this.prop = prop;
-     }
-     private By usernameField = By.xpath("//*[@id=\"hello\"]/div[2]/input");
-     private By passwordField = By.xpath("//*[@id=\"hello\"]/div[3]/input");
-     private By loginButton = By.xpath("//*[@id=\"hello\"]/div[4]/button");
-	
-	public void login(String username, String password) throws InterruptedException {
-		driver.findElement(usernameField).sendKeys("balaram@okrstar.com");
-        driver.findElement(passwordField).sendKeys("talent");
-        driver.findElement(loginButton).click();	
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//a[contains(text(),'360° feedback')]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//a[@class='dropdown-toggle']")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//a[contains(text(),'OKR for Testing')]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//h3[contains(text(),'My 360° FEEDBACK')]")).click();
-       
-        Thread.sleep(2000);
-        String noforeponsText = driver.findElement(By.xpath("(//h3[@class=\"ng-binding\"]) [5]")).getText();
-        int noforepons = Integer.parseInt(noforeponsText);
-        for (int i=4;i<= noforepons;i++) 
-        {
-//        	  JavascriptExecutor scrollup = (JavascriptExecutor) driver;
-//        	  scrollup.executeScript("window.scrollBy(0,50)");
-//        	  WebElement element = driver.findElement(By.xpath("(//div[@class=\"card list-view-card\"])[" + (i) + "]"));
 
-        	
-            JavascriptExecutor js = (JavascriptExecutor) driver;
+		 private WebDriver driver;
+	     private Properties prop;
+	     
+	     public Respons_to_the_Form(WebDriver driver, Properties prop) 
+	     {
+	         this.driver = driver;
+	         this.prop = prop;
+	     }
+	     private By usernameField = By.xpath("//*[@id=\"hello\"]/div[2]/input");
+	     private By passwordField = By.xpath("//*[@id=\"hello\"]/div[3]/input");
+	     private By loginButton = By.xpath("//*[@id=\"hello\"]/div[4]/button");
+		
+		public void login(String username, String password) throws InterruptedException {
+			driver.findElement(usernameField).sendKeys("balaram@okrstar.com");
+	        driver.findElement(passwordField).sendKeys("talent");
+	        driver.findElement(loginButton).click();	
+	        Thread.sleep(2000);
+	        driver.findElement(By.xpath("//a[contains(text(),'360° feedback')]")).click();
+	        Thread.sleep(2000);
+	        driver.findElement(By.xpath("//a[@class='dropdown-toggle']")).click();
+	        Thread.sleep(2000);
+	        driver.findElement(By.xpath("//a[contains(text(),'OKR for Testing')]")).click();
+	        Thread.sleep(2000);
+	        driver.findElement(By.xpath("//h3[contains(text(),'My 360° FEEDBACK')]")).click();
+	       
+	        Thread.sleep(2000);
+	        String noforeponsText = driver.findElement(By.xpath("(//h3[@class=\"ng-binding\"]) [5]")).getText();
+	        int noforepons = Integer.parseInt(noforeponsText);
+	        for (int i=1;i<= noforepons;i++) 
+	        {
+	        	
+	      
+	            JavascriptExecutor js = (JavascriptExecutor) driver;
 
-            // Define the XPath for the element to be found
-            String xpath = "(//div[@class='card list-view-card'])[" + (i) + "]";  // Adjust the index as necessary
+	            String xpath = "(//div[@class='card list-view-card'])[1]"; 
 
-            // Scroll until the element is visible
-            boolean elementVisible = false;
-            do {
-                try {
-                    // Try to find the element
-                    WebElement element = driver.findElement(By.xpath(xpath));
-                    
-                    // Scroll to the element
-                    js.executeScript("arguments[0].scrollIntoView(true);", element);
-                    
-                    // If no exception is thrown, element is found and visible
-                    elementVisible = true;
-                } catch (NoSuchElementException e) {
-                    // Scroll down by 50 pixels
-                    js.executeScript("window.scrollBy(0, 50);");
-                }
-            } while (!elementVisible);
+	            boolean elementVisible = false;
+	            do {
+	                try {
+	                
+	                    WebElement element = driver.findElement(By.xpath(xpath));
+	                    	                
+//	                    js.executeScript("arguments[0].scrollIntoView(true);", element);
+	                   
+	                    elementVisible = true;
+	                } catch (NoSuchElementException e) {
+	             
+	                    js.executeScript("window.scrollBy(0, 100);");
+	                }
+	            } while (!elementVisible);
 
-            // Perform actions on the element
-            WebElement element = driver.findElement(By.xpath(xpath));
-            // Example: click the element
-            element.click();
-        	
-        	
-        	
-              // Use JavaScriptExecutor to click on the element
-              JavascriptExecutor executor = (JavascriptExecutor) driver;
-              executor.executeScript("arguments[0].click();", element);
-              Thread.sleep(5000);
-  	        
+	            WebElement element = driver.findElement(By.xpath(xpath));
 
-          
-              List<WebElement> questions = driver.findElements(By.xpath("//div[@id='myokrCollapse439']//div[@class=\"form-group\"]"));
+//	            element.click();
+	        	
+	        		        
+	              JavascriptExecutor executor = (JavascriptExecutor) driver;
+	              executor.executeScript("arguments[0].click();", element);
+	              Thread.sleep(2000);
+	  	        
+	              
+	              List<WebElement> questions = driver.findElements(By.xpath("//div[@class=\"container-fluid mt-2 collapse in\"]//label[@class=\"control-label col-xs-3 ng-binding\"]"));
 
-              // Get the number of questions
-              int numberOfQuestions = questions.size();
+	              int numberOfQuestions = questions.size();
 
-              // Print the number of questions
-              System.out.println("Number of questions: " + numberOfQuestions);
-              int noofquestions = numberOfQuestions - 23;
+	              System.out.println("Number of questions: " + numberOfQuestions);
+	              int noofquestions = (numberOfQuestions - 23);
 
-              // Correct the loop condition
-              for (int i1 = 0; i1 < noofquestions; i1++) 
-              {                  
-                  Thread.sleep(1000);
-                  
-                  // Construct the XPath dynamically with the current index (i1 + 1)
-                  String radioButtonXPath = "(//input[@value=\"1 \"])[" + (i1 + 1) + "]";
-                  driver.findElement(By.xpath(radioButtonXPath)).click();
-                  Thread.sleep(1000);
-                                 
-                  js.executeScript("window.scrollBy(0, 50);");
-              }
-        
-        
-              System.out.println(noforepons);
-//   		  driver.findElement(By.xpath("(//button[@type=\"reset\"])[1]")).click();
-              driver.findElement(By.xpath("(//button[@type=\"submit\"])[" + (noofquestions) + "]")).click();
-              Thread.sleep(2000);
-              Alert alert = driver.switchTo().alert();
-              alert.accept();             
-              Thread.sleep(5000);
-              driver.findElement(By.xpath("//h3[contains(text(),'My 360° FEEDBACK')]")).click();
-              Thread.sleep(2000);
-        
-  	    }
-        
-        }
-       
-	}
-	
+	              for (int i1 = 0; i1 <= noofquestions; i1++) 
+	              {                  
+	                  Thread.sleep(1000);
+	                  
 
-	
+	                  String radioButtonXPath = "(//input[@value=\"1 \"])[" + (i1 + 1) + "]";
+	                  driver.findElement(By.xpath(radioButtonXPath)).click();
+	                  Thread.sleep(1000);
+	                                 
+//	                  js.executeScript("window.scrollBy(0, 1000);");
+	              }
+	        
+	              
+	              Thread.sleep(2000); 
+	              WebElement scrollViewElement = driver.findElement(By.xpath("(//button[@type=\"submit\"])[1]"));
+	              js.executeScript("arguments[0].scrollIntoView(true);", scrollViewElement);
+	              Thread.sleep(2000); 
+	              
+	              System.out.println(noforepons);
+//	   		  driver.findElement(By.xpath("(//button[@type=\"reset\"])[1]")).click();
+	              driver.findElement(By.xpath("(//button[@type=\"submit\"])[1]")).click();
+	              Thread.sleep(2000);
+	              Alert alert = driver.switchTo().alert();
+	              alert.accept();             
+	              Thread.sleep(5000);
+	              driver.findElement(By.xpath("//h3[contains(text(),'My 360° FEEDBACK')]")).click();
+	              Thread.sleep(2000);
+	        
+	  	    }
+	        
+	        }
+	       
+		}
+		
+
+		
 
 
